9faca370cbe725f384cf70d8c9e3424397538123
Usohatchi <eamon@itofisher.com>
2018-03-09 13:50:38 -0800

    About Me added

--- js/responsive.js
+++ js/responsive.js
@@ -24,6 +24,38 @@ $("#coverMyProjects").mouseleave(function() {
 });
 
 //About Me and My Project button clicks
+$("#coverAboutMe").click(function() {
+	if (page == "Home") {
+		//Revert Border
+		$("#aboutMe rect").attr( {"width": "2%", "x":"49%"} );
+		$("#myProjects rect").attr( {"width": "4%", "x":"48%"} );
+		
+		//Fade in About Me
+		$("#slideOff").attr("x", "-110%");
+		$("#videoHolder").css("left", "-35%").fadeTo("500", "0");
+		$("#videoHolder video").fadeTo("500", "0");
+		$("#backAbout").delay(400).fadeTo("500", "1");
+		$("#backAbout").css("left", "2.5%");
+		$("#aboutMeHolder").delay(250).animate({"box-shadow": "0px " + width/250 + "px " + width/100 + "px " + width/350 + "px rgb(7,7,7,0.2)"}, 400);
+		$("#aboutMeHolder").delay(250).fadeTo("400", "1");
+		$("#aboutMeText").css("transform", "translateY(0)");
+		$("#aboutMeTextBorder").css( {"left":"5%", "height":"36%"} );
+		page = "About Me";
+	}
+});
+$("#backAbout").click(function() {
+	if (page = "About Me") {
+		$("#slideOff").attr("x", "0");
+		$("#videoHolder").css("left", "50%").fadeTo("1500", "1");
+		$("#videoHolder video").fadeTo("1500", "1");
+		$("#backAbout").fadeTo("50", "0");
+		$("#backAbout").css("left", "-9%");
+		$("#aboutMeHolder").animate({"box-shadow": "0px 0px 0px 0px rgb(7,7,7,0.2)"}, 100).fadeTo("100", "0");
+		$("#aboutMeTextBorder").css( {"left":"-10%", "height":"0%"} );
+		$("#aboutMeText").css("transform", "translateY(-25%)");
+		page = "Home";
+	}
+});
 $("#coverMyProjects").click(function() {
 	if (page == "Home") {
 		//Revert Borders
@@ -38,9 +70,9 @@ $("#coverMyProjects").click(function() {
 			$("#slideOff").off(transEnd);
 		});
 		$("#videoHolder").css("left", "64%");
-		$("#videoHolder .current .slotHolder").delay(900).animate({"box-shadow": "0px 8px 15px 7px rgb(7,7,7,0.2)"}, 400);
-		$("#backButtonProject").delay(400).fadeTo("500", "1");
-		$("#backButtonProject").css("left", "95%");
+		$("#videoHolder .current .slotHolder").delay(900).animate({"box-shadow": "0px " + width/250 + "px " + width/100 + "px " + width/350 + "px rgb(7,7,7,0.2)"}, 400);
+		$("#backProject").delay(400).fadeTo("500", "1");
+		$("#backProject").css("left", "95%");
 		$("#projectTextHolder").delay(600).fadeTo("1000", "1");
 		$("#projectTitle").css( {"margin-top":"0", "margin-left":"0"} );
 		$("#projectTextBorder").css("height", "80%");
@@ -56,7 +88,7 @@ $("#coverMyProjects").click(function() {
 //videoNav Responsivness
 $(".videoNav").mouseenter(function() {
 	if (page == "My Projects") {
-		$(this).animate({"boxShadow": "0px " + width/500 + "px " + width/260 + "px " +  width/1000 + " rgb(7,7,7,0.6)"}, 200);
+		$(this).animate({"boxShadow": "0px " + width/500 + "px " + width/260 + "px " +  width/1500 + " rgb(7,7,7,0.6)"}, 200);
 	}
 });
 $(".videoNav").mouseleave(function() {
@@ -88,7 +120,7 @@ $("#videoHolder").on("click", ".current .videoNav", function(event) {
 			down.removeClass("down").addClass("current");
 			up.css("z-index", "-1").removeClass("up").addClass("down");
 		}
-		$("#videoHolder .current .slotHolder").delay(900).animate({"box-shadow": "0px 8px 15px 7px rgb(7,7,7,0.2)"}, 400);
+		$("#videoHolder .current .slotHolder").delay(900).animate({"box-shadow": "0px " + width/250 + "px " + width/100 + "px " + width/350 + "px rgb(7,7,7,0.2)"}, 400);
 		$(".slide").css("z-index", "0");
 	}
 });
@@ -99,14 +131,14 @@ $("video").click(function() {
 });
 
 //backButton click
-$("#backButtonProject").click(function() {
+$("#backProject").click(function() {
 	if (page == "My Projects") {
 		$("svg").css("z-index", "3");
 		$("#slideOff").attr("x", "0%");
 		$("#videoHolder").css("left", "50%");
 		$("#videoHolder .current .slotHolder").animate({"boxShadow": "0px 0px 0px 0px"}, 400);
-		$("#backButtonProject").fadeTo("50", "0");
-		$("#backButtonProject").css("left", "109%");
+		$("#backProject").fadeTo("50", "0");
+		$("#backProject").css("left", "109%");
 		$("#projectTextHolder").fadeTo("50", "0");
 		$("#projectTitle").css( {"margin-top":"-45%", "margin-left":"-10%"} );
 		$("#projectTextBorder").css("height", "0%");
